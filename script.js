65b652d76e93b86017bda9918a785e59fdb55918
paul829 <pgarofalo880@gmail,com>
2026-07-31 17:32:38 -0500

    Add grid resizing

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ function createGrid(size) {
 
     container.innerHTML = "";
 
+    let squareSize = 960/size;
+
     for (let i = 0; i < size * size; i++) {
 
         const square = document.createElement("div");
 
         square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
         square.addEventListener("mouseover", () => {
 
@@ -24,4 +29,26 @@ function createGrid(size) {
 
 createGrid(16);
 
+const resizeButton = document.querySelector("#resize-button");
+
+resizeButton.addEventListener("click", () => {
+
+
+    let size = prompt(
+        "How many squares per side?"
+
+    );
+
+    size = Number(size);
+
+    if (size > 100) {
+
+        alert("Maximum size is 100");
+
+        return;
+    }
+
+    createGrid(size);
+
+});
 
